df581c5517d8cd6d5718aa3e8e4f8145dfe00f88
ellisvelandia <eyis619@gmail.com>
2022-07-05 15:42:57 -0500

    Historia if

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # algoritmos_js
+
+En base un problema que detectes en la vida real, escribir un párrafo que lo explique. Con ello detectar las entidades y los pasos a seguir para resolverlo.
+
+Luego, escribir un código que genere los caminos posibles mediante ifs.
+
+tienen el code que hicimos en la clase para guiarse.
---
+++ main.js
@@ -0,0 +1,46 @@
+//  Las Aventuras de mike litoris
+
+// Mike Litoris tiene curiosidad acerca de la vida de adulto pero aun mayor es la curiosidad e intimdad de esa vida,
+// por tal motivo usa su laptod para saber acerca de estos temas , pero se sorprende que su red tiene este contenido
+// bloqueado por sus padres, deberia preguntarle a su padre la contraseña para poder disfrutar de contenido +18?
+// como solucionara mike litoris este dilema?
+
+// mike le pregunta a su padre directamente
+
+let respuesta = "no";
+
+if (respuesta == "si") {
+  console.log("ahora vamos a darle");
+} else if (respuesta == "no") {
+  console.log("nuestro padre, ha arruinado nuestro dia");
+} else {
+  console.log("se nos ha trabado la lengua");
+}
+
+// mike penso que deberia ganar puntos con su padre asi que en su mente decidio que con 100 puntos podria obtener la contraseña
+
+let puntos = "120";
+let logro;
+
+if (puntos > 100) {
+  logro = "Felicidades, ahora tendras el poder y el honor de ver waifus!";
+} else if (puntos <= 100) {
+  logro = "siguelo intentado , quizas lo tuyo sea una loli!";
+} else {
+  logro = "soy un santo y no hago cosas malas";
+}
+
+console.log(logro);
+
+// despues de que mike litoris logro disfrutar de su confortable contenido, a dejado evidencia en su navegador y su madre lo ha descubierto!
+// sera culpable a los ojos de su madre de sus insanos habitos?
+
+let culpable = " ";
+
+if (culpable == "si") {
+  console.log("te quedas sin internet por 1 mes por guarro");
+} else if (culpable == "no") {
+  console.log("eres un angel, de seguro fue tu padre");
+} else {
+  console.log("oh ! mierda ahora que hago!");
+}
